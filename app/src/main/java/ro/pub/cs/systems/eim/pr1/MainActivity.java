a01f4b295d64f5e2c5002e338c9e8d5af6e259ea
vlads <vlad.stanciu@qualitestgroup.com>
2026-01-19 07:41:08 +0200

    1 more commit

--- app/src/main/java/ro/pub/cs/systems/eim/pr1/MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/pr1/MainActivity.java
@@ -1,6 +1,7 @@
 package ro.pub.cs.systems.eim.pr1;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
